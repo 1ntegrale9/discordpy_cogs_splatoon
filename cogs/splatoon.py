65ad12ffb8bd88fc0537b91af6e5f0534b2352de
1ntegrale9 <st09d23+github@gm.ibaraki-ct.ac.jp>
2019-11-07 00:36:39 +0900

    Impleent: ステージ情報を取得するコマンド

--- cogs/splatoon.py
+++ cogs/splatoon.py
@@ -1,10 +1,22 @@
 from discord.ext import commands
+import aiohttp
+import io
 
 class Spla2API(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.api = 'https://spla2.yuu26.com/'
+        self.message_failure = '取得に失敗しました'
 
     @commands.command()
     async def api(self, ctx):
         await ctx.send(self.api)
+
+    @commands.command()
+    async def stage(self, ctx, p1='regular', p2='now'):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'{self.api}/{p1}/{p2}') as resp:
+                if resp.status != 200:
+                    return await ctx.send(self.message_failure)
+                stages = await resp.json()
+                await ctx.send('\n'.join(stages['result'][0]['maps']))
